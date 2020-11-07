26d39bb5478f66ba4c0f31bb8b5d2d7f3c50ec54
mbalcer <mateusz75319@gmail.com>
2020-11-07 16:33:04 +0100

    Initialize spring boot project

---
+++ announcement-system/src/main/java/pl/mbalcer/announcementsystem/AnnouncementSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.mbalcer.announcementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnouncementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnouncementSystemApplication.class, args);
+    }
+
+}
