59c624cc0986bda9daa3c7a18e70d19ea7f452ac
mbalcer <mateusz75319@gmail.com>
2020-11-08 13:23:46 +0100

    Create data to db for announcements

--- announcement-system/src/main/resources/data/users.sql
+++ announcement-system/src/main/resources/data/users.sql
@@ -2,8 +2,8 @@ INSERT INTO roles(name) VALUES('ROLE_USER');
 INSERT INTO roles(name) VALUES('ROLE_ADMIN');
 
 -- password is encode by BCrypt
--- password for mateusz: mati123
--- password for admin: admin
+-- mateusz: mati123
+-- admin: admin
 INSERT INTO users(username, email, password, enable, role_id) VALUES
 ('mateusz', 'mati@wp.pl', '$2a$10$Ya/QJWvpliz0VusAm1bqpuPaV93ba/1P1vRfiM0VUh4azSR88REC6', true, 1),
 ('admin', 'admin@admin.pl', '$2a$10$JfKYMW2Q4Cm.F5IIp9i3duSavHEYOWj28I6E4QVHEzNzX6Hf1NDcm', true , 2);
