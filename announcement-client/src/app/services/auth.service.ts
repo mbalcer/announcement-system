f9f6fafdae92a9646baf9257549914fcbf20469f
mbalcer <mateusz75319@gmail.com>
2020-11-10 19:33:40 +0100

    Implement auth and token storage services

--- announcement-client/src/app/services/auth.service.ts
+++ announcement-client/src/app/services/auth.service.ts
@@ -1,9 +1,34 @@
 import {Injectable} from '@angular/core';
+import {environment} from '../../environments/environment';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private AUTH_URL = environment.apiUrl + '/auth';
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  login(credentials): Observable<any> {
+    return this.http.post(this.AUTH_URL + 'signin', {
+      username: credentials.username,
+      password: credentials.password
+    }, this.httpOptions);
+  }
+
+  register(user): Observable<any> {
+    return this.http.post(this.AUTH_URL + 'signup', {
+      username: user.username,
+      email: user.email,
+      password: user.password
+    }, this.httpOptions);
+  }
+
 }
