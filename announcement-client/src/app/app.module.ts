c043182c6afdc3ecac9ed6c7795de5e4191b2570
mbalcer <mateusz75319@gmail.com>
2020-11-10 19:21:39 +0100

    Create login component

--- announcement-client/src/app/app.module.ts
+++ announcement-client/src/app/app.module.ts
@@ -7,10 +7,12 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material.module';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import {LoginComponent} from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
