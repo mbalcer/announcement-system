e98c7ab633a6cdb0a88eb68678238a207dc29687
mbalcer <mateusz75319@gmail.com>
2020-11-11 17:34:34 +0100

    Add auth interceptor

--- announcement-client/src/app/app.module.ts
+++ announcement-client/src/app/app.module.ts
@@ -6,10 +6,11 @@ import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material.module';
 import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
 import {HomeComponent} from './home/home.component';
+import {AuthInterceptor} from './utils/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,9 @@ import {HomeComponent} from './home/home.component';
     FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ announcement-client/src/app/utils/auth.interceptor.ts
@@ -0,0 +1,21 @@
+import {HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {TokenStorageService} from '../services/token-storage.service';
+
+const TOKEN_HEADER_KEY = 'Authorization';
+const TOKEN_PREFIX = 'Bearer ';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private token: TokenStorageService) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    let authReq = req;
+    const token = this.token.getToken();
+    if (token != null) {
+      authReq = req.clone({headers: req.headers.set(TOKEN_HEADER_KEY, TOKEN_PREFIX + token)});
+    }
+    return next.handle(authReq);
+  }
+}
