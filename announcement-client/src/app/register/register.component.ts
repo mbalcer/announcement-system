9fb64fc0ef059150a4986b14ea688f8692d34aa1
mbalcer <mateusz75319@gmail.com>
2020-11-12 20:32:18 +0100

    Add register page template

--- announcement-client/src/app/register/register.component.ts
+++ announcement-client/src/app/register/register.component.ts
@@ -1,15 +1,36 @@
 import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['../login/login.component.scss', './register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  form: any = {};
+  isSuccessfulRegister = false;
+  errorMessage = '';
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  signUp(form) {
+    this.authService.register(this.form).subscribe(
+      data => {
+        console.log(data);
+        this.isSuccessfulRegister = true;
+        this.errorMessage = '';
+        this.form = {};
+        form.submitted = false;
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isSuccessfulRegister = false;
+      }
+    );
+
   }
 
 }
--- announcement-client/src/app/services/auth.service.ts
+++ announcement-client/src/app/services/auth.service.ts
@@ -24,7 +24,7 @@ export class AuthService {
   }
 
   register(user): Observable<any> {
-    return this.http.post(this.AUTH_URL + 'signup', {
+    return this.http.post(this.AUTH_URL + '/signup', {
       username: user.username,
       email: user.email,
       password: user.password
