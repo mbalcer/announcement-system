08dbac6d1ba2843dd3b7979486f0568cb754a799
mbalcer <mateusz75319@gmail.com>
2020-11-29 22:05:12 +0100

    Create contact form template

--- announcement-client/src/app/announcement/announcement-details/announcement-details.component.ts
+++ announcement-client/src/app/announcement/announcement-details/announcement-details.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {AnnouncementService} from '../../services/announcement.service';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-announcement-details',
@@ -8,7 +9,13 @@ import {AnnouncementService} from '../../services/announcement.service';
   styleUrls: ['./announcement-details.component.scss']
 })
 export class AnnouncementDetailsComponent implements OnInit {
-  activeAnnouncement: any;
+  activeAnnouncement: any = null;
+  contactMail: any = {
+    from: '',
+    name: '',
+    messageFromUser: '',
+    announcement: null
+  };
 
   constructor(private route: ActivatedRoute, private announcementService: AnnouncementService) {
   }
@@ -20,6 +27,11 @@ export class AnnouncementDetailsComponent implements OnInit {
   getAnnouncement(id: string) {
     this.announcementService.getAnnouncement(Number(id)).subscribe(result => {
       this.activeAnnouncement = result;
+      this.contactMail.announcement = result;
     }, error => console.log(error));
+  }
+
+  sendEmail(f: NgForm) {
+    return false;
   }
 }
