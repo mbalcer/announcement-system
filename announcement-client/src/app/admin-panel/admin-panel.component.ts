d73b7f77e5a18451c7e9f29567e2fd41d228a8eb
mbalcer <mateusz75319@gmail.com>
2020-11-30 10:38:11 +0100

    Add table to manage announcements in admin panel

--- announcement-client/src/app/admin-panel/admin-panel.component.ts
+++ announcement-client/src/app/admin-panel/admin-panel.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {AnnouncementService} from '../services/announcement.service';
+import {MatTableDataSource} from '@angular/material/table';
+import {PageEvent} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-admin-panel',
@@ -6,10 +9,44 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./admin-panel.component.scss']
 })
 export class AdminPanelComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'title', 'user', 'price', 'actions'];
+  dataSource: MatTableDataSource<any[]>;
+  announcementPage: any = null;
 
-  constructor() { }
+  constructor(private announcementService: AnnouncementService) { }
 
-  ngOnInit(): void {
+  refreshTable() {
+    this.dataSource = new MatTableDataSource<any[]>(this.announcementPage.content);
   }
 
+  ngOnInit(): void {
+    const param = {
+      page: 0,
+      size: 10
+    };
+    this.getAnnouncements(param);
+  }
+
+  getAnnouncements(param) {
+    this.announcementService.getAllAnnouncements(param).subscribe(result => {
+      this.announcementPage = result;
+      this.refreshTable();
+    }, error => console.log(error));
+  }
+
+  delete(row: any) {
+    this.announcementService.deleteAnnouncement(row.id).subscribe(result => {
+      this.announcementPage.content.splice(this.announcementPage.content.indexOf(row), 1);
+      this.announcementPage.totalElements = this.announcementPage.totalElements - 1;
+      this.refreshTable();
+    }, error => console.log(error));
+  }
+
+  pagination(pageEvent: PageEvent) {
+    const param = {
+      page: pageEvent.pageIndex,
+      size: pageEvent.pageSize
+    };
+    this.getAnnouncements(param);
+  }
 }
